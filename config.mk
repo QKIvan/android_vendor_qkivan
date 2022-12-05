72846a9f8fdcfd5ab0f2e040c4f177e217bd37aa
chingsky-liu <ivan33@163.com>
2022-12-05 17:21:42 +0000

    addons: Drop Audio

--- config.mk
+++ config.mk
@@ -7,12 +7,3 @@ PRODUCT_PACKAGES += \
     PrcCalendar \
     PrcGallery2 \
     messaging
-
-# Audio
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/addons/audio,$(TARGET_COPY_OUT_PRODUCT)/media/audio)
-
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone=01_pneumatic.ogg \
-    ro.config.notification_sound=01_oi!.ogg \
-    ro.config.alarm_alert=01_bedside.ogg

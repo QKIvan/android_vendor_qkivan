1064c6fcf474b0a670a487667a105a93dac7d402
zclkkk <76833317+zclkkk@users.noreply.github.com>
2022-12-06 00:43:13 +0800

    addons: Initial import from Project-Mika
    * From https://github.com/Project-Mika/android_vendor_addons
    addons: Initial import from MikaPrebuilts
    addons: Import Nothing audio
    addons: Import NTVoiceRecorder

---
+++ config.mk
@@ -0,0 +1,16 @@
+# Apps
+PRODUCT_PACKAGES += \
+    AsusDeskClock \
+    PrcCalculator \
+    PrcGallery2 \
+    NTVoiceRecorder \
+    Via
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/addons/audio,$(TARGET_COPY_OUT_PRODUCT)/media/audio)
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.ringtone=01_pneumatic.ogg \
+    ro.config.notification_sound=01_oi!.ogg \
+    ro.config.alarm_alert=01_bedside.ogg
